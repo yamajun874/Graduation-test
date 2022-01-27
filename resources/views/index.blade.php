0bf54b050dbdfa16677f8d855734dd952ced2ff8
yamajun874 <j.yamayamada.874@gmail.com>
2022-01-27 23:38:43 +0900

    fouth-heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ mix('css/reset.css') }}">
+  <link rel="stylesheet" href="{{ mix('css/style.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
@@ -132,7 +132,7 @@
 
   @endif
 
-  <script src="{{ asset('js/main.js') }}"></script>
+  <script src="{{ mix('js/main.js') }}"></script>
 </body>
 
 </html>
